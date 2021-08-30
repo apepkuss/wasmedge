61a84afbb835aab50117d1d307a45d7b42455518
Xin Liu <xin.sam.liu@hotmail.com>
2021-08-30 11:04:03 +0000

    [refactor] (system): renamed info mod as version mod

---
+++ src/system/version.rs
@@ -0,0 +1,37 @@
+use std::ffi::CStr;
+use wasmedge_sys::ffi as we_ffi;
+
+pub fn version() -> String {
+    let c_buf = unsafe { we_ffi::WasmEdge_VersionGet() };
+    let slice: &CStr = unsafe { CStr::from_ptr(c_buf) };
+    let str_slice: &str = slice.to_str().unwrap();
+    str_slice.to_owned()
+}
+
+pub fn major_version() -> usize {
+    let major = unsafe { we_ffi::WasmEdge_VersionGetMajor() };
+    major as usize
+}
+
+pub fn minor_version() -> usize {
+    let minor = unsafe { we_ffi::WasmEdge_VersionGetMinor() };
+    minor as usize
+}
+
+pub fn version_patch() -> usize {
+    let patch = unsafe { we_ffi::WasmEdge_VersionGetPatch() };
+    patch as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_version() {
+        assert!(!version().is_empty());
+        assert!(major_version() >= 0);
+        assert!(minor_version() >= 0);
+        assert!(version_patch() >= 0);
+    }
+}
