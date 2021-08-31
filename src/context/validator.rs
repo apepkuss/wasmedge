daf3e5179d8ea4bc9f4f31953c0993534885b5b3
Xin Liu <xin.sam.liu@hotmail.com>
2021-08-31 09:45:06 +0000

    [test] (validator): added test case

--- src/context/validator.rs
+++ src/context/validator.rs
@@ -15,7 +15,7 @@ impl Validator {
         }
     }
 
-    pub fn validate(&mut self, ast_mod: &ASTModuleContext) -> WasmEdgeResult<()> {
+    pub fn validate(&self, ast_mod: &ASTModuleContext) -> WasmEdgeResult<()> {
         unsafe { check(we_ffi::WasmEdge_ValidatorValidate(self.raw, ast_mod.raw)) }
     }
 }
@@ -24,5 +24,39 @@ impl Drop for Validator {
         if !self.raw.is_null() {
             unsafe { we_ffi::WasmEdge_ValidatorDelete(self.raw) }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::context::{configure::ConfigureContext, loader::LoaderContext};
+    use crate::types::WasmEdgeProposal;
+
+    const TPATH: &str = "/root/workspace/wasmedge-ml/wasmedge/tests/data/test.wasm";
+
+    #[test]
+    fn test_validator() {
+        let mut conf = ConfigureContext::create();
+        conf.add_proposal(WasmEdgeProposal::WasmEdge_Proposal_ReferenceTypes);
+        let result = Validator::create(&conf);
+        assert!(result.is_some());
+        let validator = result.unwrap();
+
+        // load and parse file
+        let result = load_module(&conf);
+        assert!(result.is_some());
+        let ast_mod = result.unwrap();
+        assert!(!ast_mod.raw.is_null());
+
+        // validation
+        assert!(validator.validate(&ast_mod).is_ok());
+    }
+
+    fn load_module(conf: &ConfigureContext) -> Option<ASTModuleContext> {
+        let mut module = ASTModuleContext::default();
+        let mut loader = LoaderContext::create(conf)?;
+        loader.parse_from_file(&mut module, TPATH);
+        Some(module)
     }
 }
