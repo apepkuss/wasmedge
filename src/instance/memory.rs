0f9e09bfabefd7d328c1ae14e8ef466dd65dfe04
Xin Liu <xin.sam.liu@hotmail.com>
2021-09-06 08:49:28 +0000

    [refactor](memory): removed unused code

--- src/instance/memory.rs
+++ src/instance/memory.rs
@@ -72,14 +72,6 @@ impl<'store, 'vm: 'store> Drop for MemoryInstanceContext<'store, 'vm> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{
-        context::import_object::ImportObjectContext,
-        context::vm::VMContext,
-        instance::function::{FunctionTypeContext, HostFunctionContext},
-        value::*,
-    };
-    use std::ptr;
-    use wasmedge_sys::ffi as we_ffi;
 
     #[test]
     fn test_instance_memory() {
@@ -121,7 +113,7 @@ mod tests {
         let mut data_get = [];
         let result = mem_ctx.get_data(&mut data_get, 100);
         assert!(result.is_ok());
-        // ! error: xecution failed: out of bounds memory access, Code: 0x88
+        // ! error: execution failed: out of bounds memory access, Code: 0x88
         // let mut data_get = [0; 10];
         // let result = mem_ctx.get_data(&mut data_get, 65536);
         // assert!(result.is_err());
@@ -135,77 +127,5 @@ mod tests {
         // let result = mem_ctx.grow_page(2);
         // assert!(result.is_err());
         // assert_eq!(mem_ctx.page_size(), 2);
-    }
-
-    #[test]
-    fn test_wasmedge_memory_ctx() {
-        #[no_mangle]
-        unsafe extern "C" fn FuncAdd(
-            data: *mut std::os::raw::c_void,
-            mem_ctx: *mut we_ffi::WasmEdge_MemoryInstanceContext,
-            params: *const WasmEdgeValue,
-            returns: *mut WasmEdgeValue,
-        ) -> we_ffi::WasmEdge_Result {
-            let params = std::slice::from_raw_parts(params, 2);
-            let val1 = WasmEdgeValueGetI32(params[0]);
-            let val2 = WasmEdgeValueGetI32(params[1]);
-            let res = WasmEdgeValueGenI32(val1 + val2);
-            returns.write(res);
-
-            we_ffi::WasmEdge_Result { Code: 0 }
-        }
-
-        // Create the VM context.
-        let result = VMContext::create(None, None);
-        assert!(result.is_some());
-        let mut vm = result.unwrap();
-        assert!(!vm.raw.is_null());
-
-        // create import object
-        let mod_name = "calculator";
-        let mut imp_obj = ImportObjectContext::create(mod_name, ptr::null_mut()).unwrap();
-
-        // Create and add a host function instance into the import object.
-        let params = [
-            WasmEdgeValType::WasmEdge_ValType_I32,
-            WasmEdgeValType::WasmEdge_ValType_I32,
-        ];
-        let returns = [WasmEdgeValType::WasmEdge_ValType_I32];
-        let result = FunctionTypeContext::create(Some(&params), Some(&returns));
-        assert!(result.is_some());
-        let func_type = result.unwrap();
-        let result = HostFunctionContext::create(&func_type, Some(FuncAdd), 0);
-        let mut host_func = result.unwrap();
-        imp_obj.add_host_function("func-add", &mut host_func);
-
-        // register import-object
-        vm.register_module_from_import_object(&imp_obj).unwrap();
-
-        unsafe {
-            // let tf_imp_obj = we_ffi::WasmEdge_Tensorflow_ImportObjectCreate();
-            // vm_ctx
-            //     .register_module_from_import_object(&tf_imp_obj)
-            //     .unwrap();
-            // we_ffi::WasmEdge_VMRegisterModuleFromImport(vm_ctx.raw, tf_imp_obj);
-
-            // let ipt_ctx = we_ffi::WasmEdge_InterpreterCreate(ptr::null(), ptr::null_mut());
-            // let store_ctx = we_ffi::WasmEdge_StoreCreate();
-            // let result =
-            //     we_ffi::WasmEdge_InterpreterRegisterImport(ipt_ctx, store_ctx, imp_obj.raw);
-            // if !we_ffi::WasmEdge_ResultOK(result) {
-            //     println!("Import object registration failed");
-            // }
-
-            let x = we_ffi::WasmEdge_VMGetFunctionListLength(vm.raw);
-            println!("x: {}", x);
-
-            let store = we_ffi::WasmEdge_VMGetStoreContext(vm.raw);
-            // let mod_name = WasmEdgeString::from_str(mod_name).unwrap();
-            // let len = we_ffi::WasmEdge_StoreListMemoryRegisteredLength(store_ctx, mod_name.raw);
-
-            // let mod_name = WasmEdgeString::from_str("wasi_ephemeral_nn").unwrap();
-            let len = we_ffi::WasmEdge_StoreListFunctionLength(store);
-            println!("len: {}", len);
-        }
     }
 }
